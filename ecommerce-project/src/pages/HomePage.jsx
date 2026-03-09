4f0ee7ebaccd134bdaf77cee97e284a952320428
mariami57 <mmitova3@gmail.com>
2026-03-09 17:58:52 +0200

    changed the favicons for each of the pages

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import './HomePage.css';
 export function HomePage() {
   return (
     <>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <title>Ecommerce project</title>
       <Header />
       <div className="home-page">
