eb60c621bdeed31f8e5dd22141afc9297ccddc3c
mariami57 <mmitova3@gmail.com>
2026-03-10 11:07:11 +0200

    fetched data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,6 +5,12 @@ import HomeIcon from '../assets/images/home-favicon.png';
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+       return response.json();
+      }).then((data) => {
+        console.log(data);
+    });
   return (
     <>
       <link rel="icon" type="image/svg+xml" href={HomeIcon} />
