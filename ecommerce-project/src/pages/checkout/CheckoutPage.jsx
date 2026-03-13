5135520c20e39bfd288623151e38a4923c4b46ac
mariami57 <mmitova3@gmail.com>
2026-03-13 20:20:44 +0200

    generated html for the payment summary

--- ecommerce-project/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutPage.jsx
@@ -8,12 +8,18 @@ import './CheckoutPage.css';
 
 export function CheckoutPage({ cart }) {
     const [deliveryOptions, setDeliveryOptions] = useState([]);
+    const [paymentSummary, setPaymentSummary] = useState([null]);
 
     useEffect(() => {
         axios.get('/api/delivery-options?expand=estimatedDeliveryTime')
             .then((response) => {
                 setDeliveryOptions(response.data);
             });
+        
+            axios.get('/api/payment-summary')
+                .then((response) => {
+                    setPaymentSummary(response.data);
+                })
     }, []);
 
     return (
