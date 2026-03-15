312547875328a3d6e50ed2a0992c93cfe66aee87
mariami57 <mmitova3@gmail.com>
2026-03-15 12:31:29 +0200

    loading the data for the tracking page from the backend

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,33 +1,62 @@
-import { Link } from 'react-router';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { Link, useParams } from 'react-router';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import TrackingIcon from '../assets/images/tracking-favicon.png';
 import './TrackingPage.css'; 
 
+
 export function TrackingPage({ cart }) {
+
+    const { orderId, productId} = useParams();
+
+    const [order,setOrder] = useState(null);
+
+    useEffect(() => {
+        const getTrackingData = async () => {
+             const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+             setOrder(response.data)
+        }
+
+        getTrackingData();
+       
+    }, [orderId])
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) =>{
+        return orderProduct.productId === productId;
+    }
+        
+    )
     return (
         <>  
             <link rel="icon" type="image/svg+xml" href={TrackingIcon} />
             <title>Tracking</title>
             <Header cart={cart}/>
             <div className="tracking-page">
+                
                 <div className="order-tracking">
                     <Link className="back-to-orders-link link-primary" to="/orders">
                         View all orders
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        Arriving on {dayjs(orderProduct.product.estimatedDeliveryTimeMs).format('dddd MMMM D')}
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.product.quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
                         <div className="progress-label">
