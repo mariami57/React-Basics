2eb324fd3569dab4674d399bcf6fe6f26ad6102c
mariami57 <mmitova3@gmail.com>
2026-03-22 12:38:41 +0200

    added more tests

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -71,4 +71,10 @@ describe('Product component', () => {
 
         expect(loadCart).toHaveBeenCalled();
     });
+
+    it('selects a quantity', () => {
+        render(<Product product={product} loadCart={loadCart} />);
+        const quantitySelector = screen.getByTestId('product-quantity-selector');
+        expect(quantitySelector).toHaveValue('1');
+    })
 });
